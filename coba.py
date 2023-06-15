52a9cf7bbffdf43b4c3b44eb5af917a96f37357f
Aliirfan2504 <125642754+Aliirfan2504@users.noreply.github.com>
2023-06-15 09:53:00 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Aplikasi Streamlit Sederhana")
+    st.subheader("Selamat datang!")
+
+    # Tambahkan konten aplikasi di sini
+    st.write("Ini adalah aplikasi web sederhana menggunakan Streamlit.")
+
+if __name__ == '__main__':
+    main()
